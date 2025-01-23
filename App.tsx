ef108f79557820351cbfa2c7a024916aed2e80c4
NicolasOBP <nicolasobp15@gmail.com>
2025-01-23 19:46:42 -0300

    initial commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import styled from "styled-components/native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <Container>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
-    </View>
+    </Container>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
+
+const Container = styled.View`
+  background-color: #8f2727;
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
