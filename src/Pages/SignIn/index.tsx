20c2bd365ee64d0aa738aae63a5c78b3cecebaca
NicolasOBP <nicolasobp15@gmail.com>
2025-01-29 18:56:08 -0300

    up: styling Login Screen

--- src/Pages/SignIn/index.tsx
+++ src/Pages/SignIn/index.tsx
@@ -1,9 +1,41 @@
-import { Text, View } from "react-native";
+import { Platform } from "react-native";
+import {
+  AreaInput,
+  Background,
+  Container,
+  Input,
+  Link,
+  LinkText,
+  Logo,
+  SubmitButton,
+  SubmitText,
+} from "./style";
+import { useNavigation } from "@react-navigation/native";
+import { Navigate } from "@/Routes/types/screenProps";
 
 export default function SignIn() {
+  const navigation = useNavigation<Navigate>();
   return (
-    <View>
-      <Text>Tela SignIn</Text>
-    </View>
+    <Background>
+      <Container behavior={Platform.OS === "ios" ? "padding" : ""} enable>
+        <Logo source={require("@/assets/Logo.png")} />
+
+        <AreaInput>
+          <Input placeholder="Seu email" />
+        </AreaInput>
+
+        <AreaInput>
+          <Input placeholder="Sua senha" />
+        </AreaInput>
+
+        <SubmitButton activeOpacity={0.8}>
+          <SubmitText>Acessar</SubmitText>
+        </SubmitButton>
+
+        <Link onpress={() => navigation.navigate("SignUp")}>
+          <LinkText>Criar uma Conta</LinkText>
+        </Link>
+      </Container>
+    </Background>
   );
 }
