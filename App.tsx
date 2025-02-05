a458238811dcb0d4c5a54b12e64659aebba02932
NicolasOBP <nicolasobp15@gmail.com>
2025-02-05 20:22:40 -0300

    feat: dependency injection with MMKV Storage

--- App.tsx
+++ App.tsx
@@ -4,6 +4,10 @@ import { NavigationContainer } from "@react-navigation/native";
 import Routes from "@/Routes";
 import { Provider } from "react-redux";
 import { store } from "@/contexts/store";
+import initializeStorage from "@/services/storage/storageService";
+import { mmkvStorage } from "@/services/storage/mmkvStorage";
+
+initializeStorage(mmkvStorage);
 
 export default function App() {
   return (
--- src/services/storage/storageService.ts
+++ src/services/storage/storageService.ts
@@ -1,9 +1,11 @@
-import { mmkvStorage } from "./mmkvStorage";
-
 export interface StorageService {
   getItem: <T>(key: string) => Promise<T | null>;
   setItem: <T>(key: string, value: T) => Promise<void>;
   removeItem: (key: string) => Promise<void>;
 }
 
-export let storageService: StorageService = mmkvStorage;
+export let storageService: StorageService;
+
+export default function initializeStorage(service: StorageService) {
+  storageService = service;
+}
